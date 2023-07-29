9a3d29f0c5281ac7664c7d7eaa6af2d1637ce4a7
NilooFar <miss.vafayi@gmail.com>
2023-07-29 13:09:16 +0330

    split style and js files

--- assets/files/product/product.js
+++ assets/files/product/product.js
@@ -1,50 +1,5 @@
-// Sticky Header
-$(window).on('scroll', function () {
-    let scroll = $(window).scrollTop();
 
-    if (scroll >= 150) {
-        $('.top-header').addClass('none');
-        $('.bottom-header').addClass('down');
-
-    } else {
-        $('.top-header').removeClass('none');
-        $('.bottom-header').removeClass('down');
-    }
-});
-// Number Counter
-const counters = document.querySelectorAll('.value');
-const speed = 200;
-
-counters.forEach(counter => {
-    const animate = () => {
-        const value = +counter.getAttribute('akhi');
-        const data = +counter.innerText;
-
-        const time = value / speed;
-        if (data < value) {
-            counter.innerText = Math.ceil(data + time);
-            setTimeout(animate, 1);
-        } else {
-            counter.innerText = value;
-        }
-    }
-    animate();
-});
-// Course Swiper
-let swiper = new Swiper('.mySwiper', {
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 40,
-    centeredSlides: false,
-});
-// Customer Swiper
-let customerSwiper = new Swiper(".customer-swiper", {
-    effect: "cards",
-    grabCursor: true,
-  });
-
-
-  $(document).ready(function() {
+$(document).ready(function() {
     $("#toggle").click(function() {
       var elem = $(".toggle-text").text();
       if (elem == "ادامه مطالب") {
@@ -94,6 +49,4 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-
-
 
